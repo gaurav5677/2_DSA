dd7406699e915e1a6fbcd0b4e5be2c6fd365846a
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-08 13:39:30 +0530

    day 26-27 (this keyword )

--- src/_1/OOPS/_1Student.java
+++ src/_1/OOPS/_1Student.java
@@ -1,7 +1,26 @@
 package _1.OOPS;
 
 public class _1Student {
-    String name;
-    int Rollnumber;
+     public String name;
+       private int rollnumber;// here int Rollnumber is only accessible in the same package, outside the package   not accessible
+    //access Modifiers 1. public 2. private 3.default
 
+
+    public int getRollnumber(){
+        return rollnumber;
+    }
+
+//    public void setRollnumber(int num){
+//        if (num<=0){
+//            return;
+//        }
+//        rollnumber = num;
+//    }
+    public void setRollnumber(int rollnumber) {
+        if (rollnumber <= 0) {
+            return;
+        }
+       this.rollnumber = rollnumber;
+        //this
+    }
 }
--- src/_1/OOPS/_2StudentUse.java
+++ src/_1/OOPS/_2StudentUse.java
@@ -9,11 +9,13 @@ public class _2StudentUse {
         //  s1 and s2 are references to that object //not actual addresss
         System.out.println(s1);
         s1.name = "agaurav";
-        s1.Rollnumber = 45 ;
-        System.out.println(s1.name +" "+ s1.Rollnumber);
-        s2.Rollnumber=48;
+//        s1.Rollnumber = 45 ;
+        s1.setRollnumber(100);
+        System.out.println(s1.name +" "+s1.getRollnumber()); //s1.Rollnumber);
+//        s2.Rollnumber=48;
         s2.name = " adam";
-        System.out.println(s2.name + " "+ s2.Rollnumber);
+        s2.setRollnumber(-11);
+        System.out.println(s2.name + " "+s2.getRollnumber()); //s2.Rollnumber);
 
 //          _1Student is created in heap memory and s1 is stored in stack memory
     }
