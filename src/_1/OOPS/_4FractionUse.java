64823399d9c06a780975de2dbf60465ec99c7f10
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-08 20:56:50 +0530

    day 27 fraction class(Addition of two fractions)

--- src/_1/OOPS/_4FractionUse.java
+++ src/_1/OOPS/_4FractionUse.java
@@ -2,12 +2,17 @@ package _1.OOPS;
 
 public class _4FractionUse {
     public static void main(String[] args) {
-        _3fraction f1 = new _3fraction(4,6 );
+        _3fraction f1 = new _3fraction(4, 6);
 
         f1.print();
-        f1.increment();
-        f1.print();//means adding 1 to it 2/3 +1 = 2+(1*3)/3 {num+deno/deno}
+//        f1.increment();
+//        f1.print();//means adding 1 to it 2/3 +1 = 2+(1*3)/3 {num+deno/deno}
 //        //create a function to add two fraction
-        f1.add();
+        _3fraction f2 = new _3fraction(4, 8);
+       // f1.add(f2);
+        f1.print();
+
+        _3fraction f3 = _3fraction.add(f1,f2);
+        f3.print();
     }
 }
