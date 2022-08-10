155c979519d682ba2032f20a9a17a68c60120d1d
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-10 22:39:14 +0530

    day 29  (inheritance and private)

--- src/_2OOPS/car.java
+++ src/_2OOPS/car.java
@@ -1,10 +1,18 @@
 package _2OOPS;
 
+import java.awt.*;
+
 public class car extends vehicle  {
  //child class
     int numDoors;
 
-    public  void printCar(){
+    public Car(){
+        System.out.println("Car's construtor  ");
+
+    }
+
+
+    public  void print(){ //function overWriting
         System.out.println("car "+ "color "+ getColor() +" maxspeed "+ maxSpeed+" numDoors "+ numDoors );
     }
 }
---
+++ src/_3Temp/Truck.java
@@ -0,0 +1,14 @@
+package _3Temp;
+
+import _2OOPS.vehicle;
+
+
+
+public class Truck extends  vehicle{
+
+    int maxLoadingCapacity;
+
+    public  void print(){
+        System.out.println("vehicle " + " color "+ getColor() + " maxspeed " + maxSpeed + " maxLoadingCapacity ");
+    }
+}
