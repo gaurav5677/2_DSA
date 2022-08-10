e0b1d3722222c2094fdffc63a50a8ef22d65d685
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-10 22:38:59 +0530

    day 29  (inheritance and private)

---
+++ src/_3Temp/_1VehicleUse2.java
@@ -0,0 +1,9 @@
+package _3Temp;
+ import _2OOPS.vehicle;
+
+public class _1VehicleUse2 {
+    public static void main(String[] args) {
+        vehicle v = new vehicle();
+          v.maxSpeed= 10;
+    }
+}
