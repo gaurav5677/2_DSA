b67712eaecb6d15a01165faf848e8017f0612408
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-11 23:25:43 +0530

    day 29  (inheritance and private)

--- src/_2OOPS/vehicleUse.java
+++ src/_2OOPS/vehicleUse.java
@@ -3,21 +3,28 @@ package _2OOPS;
 public class vehicleUse {
     public static void main(String[] args) {
 
-        vehicle v = new vehicle();
-//        v.color ="red";
-        v.setColor("red");
-        v.maxSpeed = 80;
-        v.print();
 
-        car c = new car();
-//        c.color = " black ";
-        c.setColor("black");
-        c.maxSpeed = 123;
-        c.numDoors = 4 ;
-        c.print();
-//        c.printCar();
+        vehicle v  = new vehicle();// calling empty constructor
 
-        bicycle b = new bicycle();
-        b.print();
+
+//        car c = new car();//  this will call  the constructor of cars
+
+
+//        vehicle v = new vehicle();
+////        v.color ="red";
+//        v.setColor("red");
+//        v.maxSpeed = 80;
+//        v.print();
+//
+//        car c = new car();
+////        c.color = " black ";
+//        c.setColor("black");
+//        c.maxSpeed = 123;
+//        c.numDoors = 4 ;
+//        c.print();
+////        c.printCar();
+//
+//        bicycle b = new bicycle();
+//        b.print();
     }
 }
---
+++ src/_4Recursion1/clas.java
@@ -0,0 +1,4 @@
+package _4Recursion1;
+
+public class clas {
+}
