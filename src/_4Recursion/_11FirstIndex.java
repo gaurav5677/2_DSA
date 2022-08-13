7269a64059dc5be5c0cc2e9b911c0cb22ce6c7cb
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-13 21:12:29 +0530

    day 31 (Recursion:FirstIndex)

---
+++ src/_4Recursion/_11FirstIndex.java
@@ -0,0 +1,45 @@
+package _4Recursion;
+
+//First Index of Number
+//
+//Given an array of length N and an integer x, you need to find and return the first index of integer x present in the array. Return -1 if it is not present in the array.
+//First index means, the index of first occurrence of x in the input array.
+//Do this recursively. Indexing in the array starts from 0.
+//Input Format :
+//Line 1 : An Integer N i.e. size of array
+//Line 2 : N integers which are elements of the array, separated by spaces
+//Line 3 : Integer x
+//Output Format :
+//first index or -1
+//Constraints :
+//1 <= N <= 10^3
+//Sample Input :
+//4
+//9 8 10 8
+//8
+//Sample Output :
+//1
+public class _11FirstIndex {
+    public static void main(String[] args) {
+        int [] a = {2,3,2,5,6,7};
+        int x = 5;
+       int  startI = 0;
+        System.out.println(firstIndex(a,x,startI));
+    }
+    public static int firstIndex( int [] c , int  x , int i ){
+        if( i == c.length){
+            return -1;
+        }
+        if(c[i]==x){
+            return   i ;
+        }
+        return firstIndex( c , x ,i+1);
+
+
+    }
+
+
+    public static int firstIndex( int c [] , int b ){
+        return firstIndex(c , b , 0 );
+    }
+}
