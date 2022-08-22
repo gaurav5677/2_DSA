5716db841756eb6eb9d23f1d47be560418264dc7
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-22 23:51:55 +0530

    OPps

--- src/_1/OOPS/_4FractionUse.java
+++ src/_1/OOPS/_4FractionUse.java
@@ -5,11 +5,11 @@ public class _4FractionUse {
         _3fraction f1 = new _3fraction(4, 6);
 
         f1.print();
-//        f1.increment();
+        f1.increment();
 //        f1.print();//means adding 1 to it 2/3 +1 = 2+(1*3)/3 {num+deno/deno}
 //        //create a function to add two fraction
         _3fraction f2 = new _3fraction(4, 8);
-       // f1.add(f2);
+        f1.add(f2);
         f1.print();
 
         _3fraction f3 = _3fraction.add(f1,f2);
