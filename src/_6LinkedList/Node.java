a87a5bcbe51e6479d50fa5f90ad06b88f1882e61
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-26 19:37:28 +0530

    LinkedList

---
+++ src/_6LinkedList/Node.java
@@ -0,0 +1,12 @@
+package _6LinkedList;
+
+public class Node<T> {
+    // this node class  will have  two things data and the address of the next node
+
+
+    T data ;
+      Node<T> next;
+      Node( T data){
+          this.data = data;
+      }
+}
