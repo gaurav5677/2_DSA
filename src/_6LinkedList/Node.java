3b6d5cdff9590e8dc019badec1d6389caeb3b7b2
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-27 19:01:43 +0530

    Increment LinkedList

--- src/_6LinkedList/Node.java
+++ src/_6LinkedList/Node.java
@@ -1,6 +1,6 @@
 package _6LinkedList;
 
-public class Node<T> {
+public class Node<T> {//T means generics
     // this node class  will have  two things data and the address of the next node
 
 
---
+++ src/_6LinkedList/_5IncreamentLinkedList.java
@@ -0,0 +1,37 @@
+package _6LinkedList;
+
+
+public class _5IncreamentLinkedList {
+
+    public static void main(String[] args) {
+        Node<Integer > head = creatLinkedList();
+
+        increment(head);
+        print(head);
+    }
+    public static Node<Integer> creatLinkedList(){
+        Node<Integer> n1 = new Node<>(10);
+        Node<Integer > n2 = new Node<>(20);
+        n1.next = n2;
+         return n1;
+    }
+    public static void increment(Node<Integer> head){
+
+     Node<Integer> temp = head;
+     while(temp != null){
+         temp.data++;
+         temp= temp.next;
+     }
+    }
+
+
+    public static void print (Node<Integer> head){
+         Node<Integer> temp = head;
+         while(temp!= null){
+             System.out.println(temp.data+" ");
+             temp = temp.next;
+         }
+        System.out.println();
+    }
+
+}
