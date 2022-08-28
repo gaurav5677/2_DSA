998ce939d3b611334a5f816dda124e34a7706571
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-28 09:26:30 +0530

    Increment LinkedList

--- src/_6LinkedList/_5IncreamentLinkedList.java
+++ src/_6LinkedList/_5IncreamentLinkedList.java
@@ -13,24 +13,24 @@ public class _5IncreamentLinkedList {
         Node<Integer> n1 = new Node<>(10);
         Node<Integer > n2 = new Node<>(20);
         n1.next = n2;
-         return n1;
+        return n1;
     }
     public static void increment(Node<Integer> head){
 
-     Node<Integer> temp = head;
-     while(temp != null){
-         temp.data++;
-         temp= temp.next;
-     }
+        Node<Integer> temp = head;
+        while(temp != null){
+            temp.data++;
+            temp= temp.next;
+        }
     }
 
 
     public static void print (Node<Integer> head){
-         Node<Integer> temp = head;
-         while(temp!= null){
-             System.out.println(temp.data+" ");
-             temp = temp.next;
-         }
+        Node<Integer> temp = head;
+        while(temp!= null){
+            System.out.println(temp.data+" ");
+            temp = temp.next;
+        }
         System.out.println();
     }
 
