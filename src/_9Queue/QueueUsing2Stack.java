2863e3fd37d4f3f3651dba75e02af7429c02b7d5
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-09-08 23:44:17 +0530

    QueueUsing2Stack

---
+++ src/_9Queue/QueueUsing2Stack.java
@@ -0,0 +1,21 @@
+package _9Queue;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+
+
+public class QueueUsing2Stack {
+    public Queue<Integer> q1;
+    public Queue<Integer> q2;
+
+    public QueueUsing2Stack() {
+        this.q1 = new LinkedList<>();
+        this.q2 = new LinkedList<>();
+    }
+
+
+}
+
+
